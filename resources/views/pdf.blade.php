429c847201dd15a0e88e7a1b6147565de27bde7e
Jorge Fonseca <luizfonseca830@gmail.com>
2020-12-17 11:46:42 -0500

    correcao imagem

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -13,19 +13,18 @@
 <body>
     <div style="text-align: center; margin-top: 2%;">
 
-        <img src="{{public_path().'/images/logoinstitutosaojoseGRANDE.jpeg'}}"/>
-        <h1>Comprovante de Inscrição Para O Sortei de Vagas Ano Letivo 2021:</h1>
+        <img src="{{public_path().'/images/logoinstituto.png'}}"/>
+        <h1>Comprovante de Inscrição Para O Sortei de Vagas Ano Letivo: 2021</h1>
     </div>
     <div style="margin-left: 20%;">
         <h2>Nome: {{$comprovante->pessoa->nome_completo}}</h2>
-        <h2>Cpf:{{$comprovante->pessoa->cpf}}</h2>
+        <h2>CPF: {{$comprovante->pessoa->cpf}}</h2>
         <h2>Escolaridade: {{$comprovante->pessoa->escolaridade->nivel_escolaridade}}</h2>
         <h2>Data e Hora de Inscrição: {{date('d/m/Y H:i:s', strtotime($comprovante->pessoa->created_at))}}</h2>
-        <h2>Número de Protocolo: {{$comprovante->comprovante}}</h2>
+        <h2>Protocolo: {{$comprovante->comprovante}}</h2>
     </div>
-    <div style="margin-top: 30%; text-align: center">
-        <strong>CONGREGAÇÃO DAS SERVAS DE MARIA REPARADORAS – ACRE
-            INSTITUTO SÃO JOSÉ</strong>
-    </div>
+    <footer style="margin-top: 30%; text-align: center">
+        <strong> INSTITUTO SÃO JOSÉ – RIO BRANCO - ACRE</strong>
+    </footer>
 </body>
 </html>
