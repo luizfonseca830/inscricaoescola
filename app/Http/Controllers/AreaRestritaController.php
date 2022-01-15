a04f454fb57d098e8244776bf9e96fdf598e585e
juliorodrigues1 <juliorodrigues.ac@gmail.com>
2022-01-14 21:53:25 -0500

    realizando filtro por data

--- app/Http/Controllers/AreaRestritaController.php
+++ app/Http/Controllers/AreaRestritaController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Pessoa;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AreaRestritaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         //
-
-
-        $pessoas = Pessoa::all();
-//        dd($pessoas[0]);
+        $data_inicio = date('Y-m-d', strtotime($request->data_inicio));
+        $data_fim = date('Y-m-d', strtotime($request->data_fim));
+        $pessoas = DB::select("SELECT * FROM pessoa where DATE_FORMAT (periodo_inicio,'%Y-%m-%d') BETWEEN '$data_inicio' AND '$data_fim'");
         return view('pages.visualizacao')->with('pessoas', $pessoas);
+    }
+
+    public function filter()
+    {
+        return view('pages.filter');
     }
 }
