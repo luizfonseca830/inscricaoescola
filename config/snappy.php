1bf7e46b0f94159813ad680c0c71764943eef68b
root <root@vps-5744947.csmracre.com.br>
2022-01-15 13:20:48 -0300

    adicionando depen do pdf

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
+        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
