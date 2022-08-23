ad90d05c0d8d55e1ee95ae54e1b0c272b9245f7b
Jorge <luizfonseca830>
2022-08-23 20:05:35 -0300

    removido chart e bibliotecas que não usadas, trouxe javascript do chart para o projeto, alterei o yaml do docker deixando dinamico o banco e a senha pegando do env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
-use App\Charts\InscricoesChart;
 use Illuminate\Support\ServiceProvider;
-use ConsoleTVs\Charts\Registrar as Charts;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,13 +21,4 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(Charts $charts)
-    {
-        Schema::defaultStringLength(191);
-
-        //
-        $charts->register([
-            InscricoesChart::class
-        ]);
-    }
 }
