42c8073bfc801f537f2c36e88087761614ed2b31
Jorge <luizfonseca830>
2022-08-23 20:44:37 -0300

    correções devido a erro no pipeline

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -3,18 +3,17 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class CustomerTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    use RefreshDatabase;
-
-    public function testBasicTest()
+    public function test_example()
     {
         $response = $this->get('/');
 
